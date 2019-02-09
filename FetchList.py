3a37296f334b6ef7fa6db920b0ca14877fb9e39d
Sean Elliott <elliottqsean@gmail.com>
2019-02-09 14:36:43 -0800

    Add extractVocabulary into two lists

--- FetchList.py
+++ FetchList.py
@@ -1,16 +1,37 @@
 import requests
+from bs4 import BeautifulSoup
 
 class FetchList:
 
-  def __init__(self):
+  def __init__(self, listId, listName):
     self.domain   = 'http://spanishdict.com/lists/'
     self.dstName  = 'palabras.html'
     self.page     = None
+    self.spanishVocab = []
+    self.englishVocab = []
 
-  def getList(self, listId, listName):
-    url = self.formUrl(listId,listName)
+    self.listId = listId
+    self.listName = listName
+
+  """
+    Helper function to make GET request
+  """
+  def getList(self):
+    url = self.formUrl(self.listId,self.listName)
     self.page = requests.get(url)
 
+  """
+    Helper function to form URL
+  """
   def formUrl(self, listId, listName):
     return self.domain + str(listId) + '/' + str(listName)
 
+  """
+    Main method to retrieve and parse vocabulary web page
+  """
+  def extractVocabulary(self):
+    self.getList()
+    
+    soup = BeautifulSoup(self.page.content, 'html.parser')
+    self.spanishVocab = [ word.string for word in soup.find_all("span","source--1F2Ol")] 
+    self.englishVocab = [ word.string for word in soup.find_all("span","translation--2F5zL")]
--- TestFetchList.py
+++ TestFetchList.py
@@ -5,7 +5,9 @@ from FetchList import FetchList
 class TestFetchList(unittest.TestCase):
 
   def setUp(self):
-    self.fetcher = FetchList()
+    self.testListId = '148070'
+    self.testListName = 'verbs-followed-by-a'
+    self.fetcher = FetchList(self.testListId, self.testListName)
 
   def test_formUrl_success(self):
     listId      = 1234
@@ -16,10 +18,18 @@ class TestFetchList(unittest.TestCase):
     self.assertEqual(expectedUrl,testUrl)
 
   def test_getList_200(self):
-    testListId = '148070'
-    testListName = 'verbs-followed-by-a'
-    self.fetcher.getList(testListId, testListName)
+    self.fetcher.getList()
     self.assertEqual(self.fetcher.page.status_code, 200)
+
+  def test_processTestPage_success(self):
+    spanishExpected = ['bajar', 'acercarse']
+    englishExpected = ['to go down', 'to approach']
+
+    self.fetcher = FetchList(self.testListId, self.testListName)
+    self.fetcher.extractVocabulary()
+
+    self.assertEqual(spanishExpected, self.fetcher.spanishVocab)
+    self.assertEqual(englishExpected, self.fetcher.englishVocab)
 
 if __name__ == '__main__':
   unittest.main()
