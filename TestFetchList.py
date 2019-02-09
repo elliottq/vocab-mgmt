65d075579cc24aad3603763c04f27c984c407a39
Sean Elliott <elliottqsean@gmail.com>
2019-02-09 14:05:26 -0800

    Add testing

---
+++ TestFetchList.py
@@ -0,0 +1,26 @@
+import unittest
+
+from FetchList import FetchList
+
+class TestFetchList(unittest.TestCase):
+
+  def setUp(self):
+    self.fetcher = FetchList()
+
+  def test_formUrl_success(self):
+    listId      = 1234
+    listName    = 'foo'
+    expectedUrl = 'http://spanishdict.com/lists/1234/foo'
+    
+    testUrl = self.fetcher.formUrl(listId, listName)
+    self.assertEqual(expectedUrl,testUrl)
+
+  def test_getList_200(self):
+    testListId = '148070'
+    testListName = 'verbs-followed-by-a'
+    self.fetcher.getList(testListId, testListName)
+    self.assertEqual(self.fetcher.page.status_code, 200)
+    
+
+if __name__ == '__main__':
+  unittest.main()
