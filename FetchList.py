67c6bb0b716579126f28749d7c39c6dd092bea7f
Sean Elliott <elliottqsean@gmail.com>
2019-02-09 14:05:48 -0800

    Make FetchList class

--- FetchList.py
+++ FetchList.py
@@ -1,19 +1,22 @@
 import requests
 
-url='https://www.spanishdict.com/lists/146312/las-palabras'
+#### DELETE THIS
+#url='https://www.spanishdict.com/lists/146312/las-palabras'
+#
+#print("Get the web page...")
+#page = requests.get(url)
+#
+#fileName='palabras.html'
+#
+#with open(fileName,'w') as dst:
+#  dst.write(page.content)
 
-print("Get the web page...")
-page = requests.get(url)
 
-fileName='palabras.html'
-with open(fileName,'w') as dst:
-  dst.write(page.content)
-
-
-class FetchList():
+class FetchList:
 
   def __init__(self):
     self.domain   = 'http://spanishdict.com/lists/'
+    self.dstName  = 'palabras.html'
     self.page     = None
 
   def getList(self, listId, listName):
@@ -21,4 +24,9 @@ class FetchList():
     self.page = requests.get(url)
 
   def formUrl(self, listId, listName):
-    return self.domain + listId + '/' + 'listName' 
+    return self.domain + str(listId) + '/' + str(listName)
+
+  def saveListWebPage():
+    with open(self.dstName, 'w') as dst:
+      dst.write(self.page.content)
+
