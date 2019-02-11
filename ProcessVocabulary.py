3c089fce82b0b16e3d94eaa6596ec96408e77a0d
Sean Elliott <elliottqsean@gmail.com>
2019-02-10 17:12:49 -0800

    Move a finalized csv file to dropbox

--- ProcessVocabulary.py
+++ ProcessVocabulary.py
@@ -1,20 +1,43 @@
 import os
+from shutil import copyfile
 
 from FetchList import FetchList
 
 class ProcessVocabulary:
 
   def __init__(self, vocabulary, fileName, delimiter = '\t'):
+    self.dropboxPath = '/home/sean/Dropbox/Apps/Flashcards Deluxe/'
     self.vocabulary = vocabulary
     self.fileName = fileName
     self.delimiter = delimiter
 
+    self.curPath = str(os.path.abspath(os.curdir))
+
   def savePairs(self):
-     curpath = os.path.abspath(os.curdir)
-     dstDir  = str(curpath) + '/resource/'
+     dstDir  = self.curPath + '/resource/'
      fileLoc = dstDir + self.fileName
      print "Writing pairs to ", fileLoc
      with open(fileLoc, 'w') as myFile:
        for self.spanish, self.english in self.vocabulary:
          myFile.write(self.spanish.encode("UTF-8") + self.delimiter + self.english.encode("UTF-8"))
          myFile.write("\n")
+
+  def moveToDropBox(self):
+    print "Final move to DropBox..."
+    finalFileSrc = self.curPath + '/resource/'  + self.fileName
+    finalFileDst = self.dropboxPath + self.fileName
+    copyfile(finalFileSrc, finalFileDst)
+
+
+
+if __name__ == '__main__':
+    listId = '146312'
+    listName = 'las-palabras'
+    fileName = 'test.csv'
+        
+    fetcher = FetchList(listId,listName)
+    fetcher.extractVocabulary()
+
+    processor = ProcessVocabulary(fetcher.getPairs(), fileName)
+    processor.savePairs()
+    processor.moveToDropBox()
--- TestProcessVocabulary.py
+++ TestProcessVocabulary.py
@@ -1,5 +1,6 @@
 import unittest
 import filecmp
+import os
 
 from FetchList import FetchList
 from ProcessVocabulary import ProcessVocabulary
@@ -9,12 +10,16 @@ class TestProcessVocabulary(unittest.TestCase):
   def test_writeTestList_success(self):
     fileName = 'tmpPairs.csv'
     basePath = '/home/sean/code/vocab-mgmt/resource/'
+    tmpFileLoc = basePath + fileName
+    tstFileLoc = basePath + 'testPairs.csv'
+
     fetcher = FetchList('148070', 'verbs-followed-by-a')
     fetcher.extractVocabulary()
     
     processor = ProcessVocabulary(fetcher.getPairs(), fileName)
     processor.savePairs()
-    self.assertTrue(filecmp.cmp(basePath + fileName, basePath + 'testPairs.csv'))
+    self.assertTrue(filecmp.cmp(tmpFileLoc, tstFileLoc))
+    os.remove(tmpFileLoc)
     
 
 if __name__ == '__main__':
